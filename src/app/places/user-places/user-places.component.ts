2d67a7dcedc0a0abc0ee31d2c49f67ee6a094d4d
Milton Munoz <livemilton@gmail.com>
2025-10-29 22:00:12 -0500

    angular-Http request: :sparkles: sending DELETE requests

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -15,6 +15,7 @@ import { PlacesService } from '../places.service';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent implements OnInit {
+
   // places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
@@ -39,4 +40,12 @@ export class UserPlacesComponent implements OnInit {
       subscription.unsubscribe();
     });
   }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+}
 }
